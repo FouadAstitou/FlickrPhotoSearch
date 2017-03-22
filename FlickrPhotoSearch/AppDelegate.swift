a7bf0b83b7e95322298d3eba7ea07cee49068018
Fouad <f.astitou@gmail.com>
2017-03-22 23:10:09 +0100

    - Updated the Appdelegate to initiate dependency injection.

--- FlickrPhotoSearch/AppDelegate.swift
+++ FlickrPhotoSearch/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UINavigationController(rootViewController: photosViewController)
         
         // Dependency injection.
-        photosViewController.photoStore = PhotoStore()
+        photosViewController.photoStore = PhotoStore() 
         
         return true
     }
